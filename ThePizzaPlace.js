ccfb3936e4653193de072bafe505ae5f3b5185c7
Brandon <brandonderrill@gmail.com>
2021-07-06 12:01:52 -0500

    ThePizzaPlace.js

---
+++ ThePizzaPlace.js
@@ -0,0 +1,10 @@
+const PizzaPlace = "Hood's Pizza";
+console.log(PizzaPlace);
+let num1 = 3;
+console.log(num1);
+console.log(`${PizzaPlace} now offers 1 Large Pizza with ${num1} toppings`);
+let toppings = 10
+//toppings
+//if you offer more than 10 toppings: "A whole lot of pizza."
+if (toppings >= 10 && toppings < 20)
+    console.log(`"A whole lot of pizza."`)
